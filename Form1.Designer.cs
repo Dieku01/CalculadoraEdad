898bb8ee1a250dd73ca7d91ebc20cbdec2afff35
Dieku01 <solgaleon2704@gmail.com>
2020-11-04 22:45:07 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CalcularEdad
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Edad: ";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(115, 180);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(122, 30);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "&Calcular edad";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(188, 59);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2020, 11, 4, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(137, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.Value = new System.DateTime(2020, 11, 4, 0, 0, 0, 0);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(79, 121);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(25, 25);
+            this.lblEdad.TabIndex = 8;
+            this.lblEdad.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 18);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Fecha de nacimiento:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 233);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmPrincipal";
+            this.Text = "Calculadora de edad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularEdad
+{
+    public partial class frmPrincipal : Form
+    {
+        int edadYear;
+        int edadMonth;
+        int edadDay;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lblEdad.Text = dateTimePicker1.Value.Day.ToString() + "- " + dateTimePicker1.Value.Month.ToString() + "- " + dateTimePicker1.Value.Year.ToString();
+            if (int.Parse(dateTimePicker1.Value.Year.ToString()) > int.Parse(DateTime.Now.Year.ToString()))
+            {
+                MessageBox.Show("Error en la selección de edad", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                edadYear = DateTime.Now.Year - dateTimePicker1.Value.Year;
+                if (int.Parse(dateTimePicker1.Value.Month.ToString()) > int.Parse(DateTime.Now.Month.ToString()))
+                {
+                    MessageBox.Show("Error en la selección de edad", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (int.Parse(dateTimePicker1.Value.Month.ToString()) == int.Parse(DateTime.Now.Month.ToString()))
+                {
+                    if (int.Parse(dateTimePicker1.Value.Day.ToString()) > int.Parse(DateTime.Now.Day.ToString()))
+                    {
+                        MessageBox.Show("Error en la selección de edad", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        edadDay = DateTime.Now.Day - dateTimePicker1.Value.Day;
+                    }
+                }
+                else
+                {
+                    edadMonth = DateTime.Now.Month - dateTimePicker1.Value.Month;
+                    edadDay = DateTime.Now.Day - dateTimePicker1.Value.Day;
+                    if (DateTime.Now.Day > dateTimePicker1.Value.Day)
+                    {
+                        edadDay = DateTime.Now.Day - dateTimePicker1.Value.Day;
+                    }
+                    else
+                    {
+                        edadDay = dateTimePicker1.Value.Day -DateTime.Now.Day;
+                    }
+                }
+            }
+            lblEdad.Text = $"{edadYear.ToString()} Año/s {edadMonth.ToString()} Mes/es {edadDay.ToString()} Día/s";
+        }
+    }
+}
